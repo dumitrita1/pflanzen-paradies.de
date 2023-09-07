18427e5cdcf2af8cd6c8cec1af1c3b71a56b7587
dumitrita1 <d.gorgan@hks-agentur.de>
2023-09-07 14:58:29 +0200

    PHP in HTML eingegeben

---
+++ deko/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Pflanzen Paradies</title>
+    </head>
+    <body>
+        <?php include "../includes/header.php" ?>
+
+        <main>
+            <h1>
+            Die schönsten Dekorationen für Ihre Pflanzen
+            </h1>
+        </main>
+
+        <?php include "../includes/footer.php" ?>
+
+    </body>
+</html>
